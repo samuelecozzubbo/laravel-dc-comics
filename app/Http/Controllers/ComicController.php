b4258ffcf791b3a28f0d143cbf977786c4a9151a
samuelecozzubbo <samuele.cozzubbo@gmail.com>
2024-09-18 16:25:37 +0200

    Fix: create new comics with fillable

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -63,13 +63,14 @@ class ComicController extends Controller
         //Creazione Nuovo fumetto
         $data = $request->all();
         $new_comic = new Comic();
-        $new_comic->title = $data['title'];
-        $new_comic->description = $data['description'];
-        $new_comic->thumb = $data['thumb'];
-        $new_comic->price = $data['price'];
-        $new_comic->series = $data['series'];
-        $new_comic->sale_date = $data['sale_date'];
-        $new_comic->type = $data['type'];
+        // $new_comic->title = $data['title'];
+        // $new_comic->description = $data['description'];
+        // $new_comic->thumb = $data['thumb'];
+        // $new_comic->price = $data['price'];
+        // $new_comic->series = $data['series'];
+        // $new_comic->sale_date = $data['sale_date'];
+        // $new_comic->type = $data['type'];
+        $new_comic->fill($data);
         $new_comic->save();
         return redirect()->route('comics.show', $new_comic->id);
     }
